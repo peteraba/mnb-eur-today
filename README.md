cf47a839759ded83c26656dcc098a2be77f767e9
Peter Aba <peter@peteraba.com>
2019-05-21 13:14:02 +0200

    Slim endpoint wrapping wround exchange rates

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# mnb-eur-today
-Current EUR-HUF exchange rate on MNB
+# Slim MNB Exchange Rate
+
+Exchange rates from [mnb.hu](https://mnb.hu/).
+
+### Why does this exist?
+
+1. MNB only provides a SOAP interface.
+2. Go, and many other technology stacks, does not come with good SOAP support.
+
+### How does it work?
+
+Using [MNB - Exchange Rate library](https://github.com/icetee/mnb-exchange-rate) and providing a simple
+[Slim framework endpoint](https://www.slimframework.com/) wrapper around it.
+
+### How can I run this?
+
+1. Create a simple `.env` file containing a line with an `MNB_PASSWORD` variable
+2. Start up the docker images with `docker-compose up -d`
+
+### Examples
+
+Using curl to retrieve the current EUR-HUF rates
+```
+> curl http://127.0.0.1:8023\?p\=abcd
+2019-05-21	EUR	1	327.19
+```
+
+Using curl to retrieve given exchange rates
+```
+> curl http://127.0.0.1:8023\?p\=abcd\&startDate\=2019-05-01\&endDate\=2019-05-05\&currencies\=EUR,USD 
+2019-05-03	EUR	1	323.82
+2019-05-03	USD	1	290.03
+2019-05-02	EUR	1	324.32
+2019-05-02	USD	1	289.31
+```
+
+Using curl to retrieve given exchange rates with semicolon as separator
+```
+> curl http://127.0.0.1:8023\?p\=abcd\&delimeter\=\;\&startDate\=2019-05-01\&endDate\=2019-05-05\&currencies\=EUR,USD
+2019-05-03;EUR;1;323.82
+2019-05-03;USD;1;290.03
+2019-05-02;EUR;1;324.32
+2019-05-02;USD;1;289.31
+```
--- index.php
+++ index.php
@@ -2,10 +2,103 @@
 
 declare(strict_types=1);
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 require "vendor/autoload.php";
+
+const MNB_PASSWORD = 'MNB_PASSWORD';
+
+const ONE_DAY_IN_HOURS = 24 * 60 * 60;
 
 $exchangeService = new \Icetee\MNB\ExchangeRate();
 
-$currentRate = $exchangeService->getCurrentExchangeRate('EUR');
+$app = new \Slim\App;
 
-echo $currentRate->getValue();
+$app->add(
+    function (Request $request, Response $response, callable $next) {
+        $params = $request->getQueryParams();
+
+        if (empty($params['p']) || !getenv(MNB_PASSWORD) || $params['p'] !== getenv(MNB_PASSWORD)) {
+            return $response->withStatus(\Slim\Http\StatusCode::HTTP_UNAUTHORIZED);
+        }
+
+        return $next($request, $response);
+    }
+);
+
+$app->get(
+    '/',
+    function (Request $request, Response $response) use ($exchangeService) {
+        $params = $request->getQueryParams();
+
+        $startDate  = empty($params['startDate']) ? date('Y-m-d') : $params['startDate'];
+        $endDate    = empty($params['endDate']) ? date('Y-m-d') : $params['endDate'];
+        $currencies = empty($params['currencies']) ? 'EUR' : $params['currencies'];
+        $delimeter  = empty($params['delimeter']) ? "\t" : $params['delimeter'];
+
+        // Request validation
+        try {
+            $today = new DateTime();
+
+            $sd = new DateTime($startDate);
+            if ($sd > $today) {
+                $response->getBody()->write(sprintf('Invalid startDate: %s', $startDate));
+
+                return $response->withStatus(\Slim\Http\StatusCode::HTTP_BAD_REQUEST);
+            }
+
+            $ed = new DateTime($endDate);
+            if ($ed > $today || $ed < $sd) {
+                $response->getBody()->write(sprintf('Invalid endDate: %s', $endDate));
+
+                return $response->withStatus(\Slim\Http\StatusCode::HTTP_BAD_REQUEST);
+            }
+
+            if (!preg_match('/^([A-Z]{3}\,)*$/Ums', $currencies . ',')) {
+                $response->getBody()->write(sprintf('Invalid currencies: %s', $currencies));
+
+                return $response->withStatus(\Slim\Http\StatusCode::HTTP_BAD_REQUEST);
+            }
+
+            if (strlen($delimeter) > 100) {
+                $response->getBody()->write(sprintf('Invalid delimeter: %s', $delimeter));
+
+                return $response->withStatus(\Slim\Http\StatusCode::HTTP_BAD_REQUEST);
+            }
+        } catch (\Exception $e) {
+            $response->getBody()->write(sprintf('Unexpected error during validation'));
+
+            return $response->withStatus(\Slim\Http\StatusCode::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // Retrieve data
+        /** @var \Icetee\MNB\RateEntityCollection $rates */
+        try {
+            $rates = $exchangeService->getExchangeRates($startDate, $endDate, $currencies);
+        } catch (\Exception $e) {
+            $response->getBody()->write(sprintf('Internal error: %s', $e->getMessage()));
+
+            return $response->withStatus(\Slim\Http\StatusCode::HTTP_BAD_REQUEST);
+        }
+
+        // Create response
+        $lines = '';
+        /** @var \Icetee\MNB\RateEntity $rate */
+        foreach ($rates->getCollection() as $rate) {
+            $lines .= sprintf(
+                "%s$delimeter%s$delimeter%s$delimeter%s\n",
+                $rate->getDate(),
+                $rate->getCurrency(),
+                $rate->getUnit(),
+                $rate->getValue()
+            );
+        }
+
+        $response->getBody()->write($lines);
+
+        return $response->withHeader('Content-type', 'text/csv');
+    }
+);
+
+$app->run();
